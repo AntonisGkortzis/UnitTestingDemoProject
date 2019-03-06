53e982494f317e92354a7d07898e02a88ab1d5d6
Antonis Gkortzis <antonis.gkortzis@gmail.com>
2019-03-06 16:39:54 +0200

    Add travis tools

---
+++ tools/travis-deploy.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#
+# CI script to deploy the material on the build site
+#
+
+set -e # Exit with nonzero exit code if anything fails
+
+if [ -n "$DEBUG" ] ; then
+  set -x
+fi
+
+SOURCE_BRANCH="master"
+TARGET_BRANCH="gh-pages"
+
+function doCompile {
+  gulp rebuild
+}
+
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; just doing a build."
+    doCompile
+    exit 0
+fi
+
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
+# Clone the existing gh-pages for this repo into a second working repository
+# in the build/ directory.
+# Create a new empty branch if gh-pages doesn't exist yet
+# (should only happen on first deploy)
+git clone $REPO build
+cd build
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+cd ..
+
+# Clean build existing contents
+rm -rf build/* || exit 0
+
+# Run our compile script
+doCompile
+
+# Now let's go have some fun with the cloned repo
+cd build
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+# If there are no changes to the compiled build (e.g. this is a README update) then just bail.
+if git diff --quiet ; then
+    echo "No changes to the output on this push; exiting."
+    exit 0
+fi
+
+# Commit the "changes", i.e. the new version.
+# The delta will show diffs between new and old versions.
+git add --all .
+git commit -m "Deploy to GitHub Pages: ${SHA}"
+
+# Don't display credentials
+set +x
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $encrypted_bf13b9e06e68_key -iv $encrypted_bf13b9e06e68_iv -in ../deploykeySEiP.enc -out deploykeySEiP -d
+openssl aes-256-cbc -K $encrypted_1759e8df177c_key -iv $encrypted_1759e8df177c_iv -in ../deploy_key_cv.enc -out deploy_key_cv -d
+
+if [ -n "$DEBUG" ] ; then
+  set -x
+fi
+
+chmod 600 deploy_key_cv
+eval `ssh-agent -s`
+ssh-add deploy_key_cv
+
+# Now that we're all set up, we can push.
+git push $SSH_REPO $TARGET_BRANCH
